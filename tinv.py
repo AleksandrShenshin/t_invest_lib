5bfb3554ae79dc9a8c3e15350e4d69a2e66524ad
Aleksandr Shenshin <aashenshin@yandex.ru>
2026-02-08 20:26:43 +0300

    Change API tinkoff to t_tech

--- tinv.py
+++ tinv.py
@@ -1,6 +1,6 @@
 import asyncio
 from decouple import config
-from tinkoff.invest import Client, InstrumentIdType
+from t_tech.invest import Client, InstrumentIdType
 
 async def get_figi_instrument(ticker, class_code):
     with Client(config('T_TOKEN')) as client:
@@ -13,5 +13,4 @@ async def get_figi_instrument(ticker, class_code):
         except:
             return -1, None, f"ERROR: get_figi_instrument(): get_instrument_by({ticker}, {class_code})"
         else:
-            print(instrument_response.instrument.figi, flush=True)
             return 0, instrument_response.instrument.figi, None
