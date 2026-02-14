8aa333355e4b1807450d5c2b82abd69791448a76
Aleksandr Shenshin <aashenshin@yandex.ru>
2026-02-14 18:54:22 +0300

    Change func get_figi_instrument() to get_param_instrument()

--- tinv.py
+++ tinv.py
@@ -1,16 +1,30 @@
 import asyncio
 from decouple import config
 from t_tech.invest import Client, InstrumentIdType
+from t_tech.invest.exceptions import RequestError
+from t_tech.invest.utils import quotation_to_decimal
 
-async def get_figi_instrument(ticker, class_code):
+async def get_param_instrument(ticker_instr):
+    ticker_param = {'ticker': '', 'figi': '', 'precision': ''}
     with Client(config('T_TOKEN')) as client:
-        try:
-            instrument_response = client.instruments.get_instrument_by(
-                id_type=InstrumentIdType.INSTRUMENT_ID_TYPE_TICKER,
-                id=ticker,
-                class_code=class_code,
-            )
-        except:
-            return -1, None, f"ERROR: get_figi_instrument(): get_instrument_by({ticker}, {class_code})"
+        for ticker in [ticker_instr, ticker_instr.upper()]:
+            for class_code in ["SPBFUT", "TQBR"]:
+                try:
+                    instrument_response = client.instruments.get_instrument_by(
+                        id_type=InstrumentIdType.INSTRUMENT_ID_TYPE_TICKER,
+                        id=ticker,
+                        class_code=class_code
+                    )
+                    break
+                except RequestError as e:
+                    continue
+            else:
+                continue
+            break
         else:
-            return 0, instrument_response.instrument.figi, None
+            return -1, ticker_param, f"ERROR: get_param_instrument(): get_instrument_by({ticker_instr})"
+
+        ticker_param['ticker'] = instrument_response.instrument.ticker
+        ticker_param['figi'] = instrument_response.instrument.figi
+        ticker_param['precision'] = quotation_to_decimal(instrument_response.instrument.min_price_increment)
+        return 0, ticker_param, None
