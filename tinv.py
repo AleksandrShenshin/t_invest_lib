5ff7d9c05ead975914a80d48196564d9e09af563
Aleksandr Shenshin <aashenshin@yandex.ru>
2026-02-19 23:54:39 +0300

    Add stream_ticker_one_minute()

--- tinv.py
+++ tinv.py
@@ -3,6 +3,14 @@ from decouple import config
 from t_tech.invest import Client, InstrumentIdType
 from t_tech.invest.exceptions import RequestError
 from t_tech.invest.utils import quotation_to_decimal
+from t_tech.invest import (
+    AsyncClient,
+    CandleInstrument,
+    MarketDataRequest,
+    SubscribeCandlesRequest,
+    SubscriptionAction,
+    SubscriptionInterval,
+)
 
 async def get_param_instrument(ticker_instr):
     ticker_param = {'ticker': '', 'figi': '', 'precision': ''}
@@ -28,3 +36,46 @@ async def get_param_instrument(ticker_instr):
         ticker_param['figi'] = instrument_response.instrument.figi
         ticker_param['precision'] = quotation_to_decimal(instrument_response.instrument.min_price_increment)
         return 0, ticker_param, None
+
+
+async def stream_ticker_one_minute(lock, shared_tasks, ticker):
+    async with lock:
+        figi = shared_tasks[ticker]['figi']
+
+    async def request_iterator():
+        yield MarketDataRequest(
+            subscribe_candles_request=SubscribeCandlesRequest(
+                subscription_action=SubscriptionAction.SUBSCRIPTION_ACTION_SUBSCRIBE,
+                instruments=[
+                    CandleInstrument(
+                        figi=figi,
+                        interval=SubscriptionInterval.SUBSCRIPTION_INTERVAL_ONE_MINUTE,
+                    )
+                ],
+            )
+        )
+        while True:
+            await asyncio.sleep(1)
+
+    async with AsyncClient(config('T_TOKEN')) as client:
+        async for marketdata in client.market_data_stream.market_data_stream(
+                request_iterator()
+        ):
+            if marketdata.candle:
+                # Данные — свечи
+                async with lock:
+                    candle = marketdata.candle
+                    try:
+                        shared_tasks[ticker]['high'] = float(quotation_to_decimal(candle.high))
+                        shared_tasks[ticker]['low'] = float(quotation_to_decimal(candle.low))
+                        shared_tasks[ticker]['volume'] = int(candle.volume)
+                        shared_tasks[ticker]['time_received'] = candle.last_trade_ts
+                    except ValueError:
+                        pass
+
+                    if not shared_tasks[ticker]['depends']:
+                        # Все задачи с ticker завершены - завершаем опрос данного ticker
+                        shared_tasks.pop(ticker, None)
+                        break
+
+                await asyncio.sleep(3)
