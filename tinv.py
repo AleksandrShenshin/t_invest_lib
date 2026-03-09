aea76f66c6888b58cf1f0e1a16475dc4a67d2af2
Aleksandr Shenshin <aashenshin@yandex.ru>
2026-03-10 00:00:23 +0300

    Add stream_list_figi_five_minute()

--- tinv.py
+++ tinv.py
@@ -104,3 +104,94 @@ async def stream_ticker_one_minute(lock, shared_tasks, ticker):
                             print(f"ERROR stream_ticker_one_minute({ticker}): {ticker} not found in shared_tasks = {shared_tasks}", flush=True)
     except Exception as e:
         print(f"ERROR critical stream_ticker_one_minute({ticker}): {e}", flush=True)
+
+
+async def stream_list_figi_five_minute(lock_data_long5, data_tasks_long5, market):
+    should_unsubscribe = False
+    async with lock_data_long5:
+        list_figi = list(data_tasks_long5[market]['tickers'])
+
+    async def request_iterator():
+        yield MarketDataRequest(
+            subscribe_candles_request=SubscribeCandlesRequest(
+                subscription_action=SubscriptionAction.SUBSCRIPTION_ACTION_SUBSCRIBE,
+                instruments=[
+                    CandleInstrument(
+                        figi=figi,
+                        interval=SubscriptionInterval.SUBSCRIPTION_INTERVAL_FIVE_MINUTES,
+                    )
+                    for figi in list_figi
+                ],
+            )
+        )
+
+        while True:
+            if should_unsubscribe:
+                # Отмена подписки
+                yield MarketDataRequest(
+                    subscribe_candles_request=SubscribeCandlesRequest(
+                        subscription_action=SubscriptionAction.SUBSCRIPTION_ACTION_UNSUBSCRIBE,
+                        instruments=[
+                            CandleInstrument(
+                                figi=figi,
+                                interval=SubscriptionInterval.SUBSCRIPTION_INTERVAL_FIVE_MINUTES,
+                            )
+                            for figi in list_figi
+                        ],
+                    )
+                )
+                break
+            await asyncio.sleep(1)
+
+    try:
+        async with AsyncClient(config('T_TOKEN')) as client:
+            async for marketdata in client.market_data_stream.market_data_stream(
+                    request_iterator()
+            ):
+                if marketdata.candle:
+                    # Данные — свечи
+                    async with lock_data_long5:
+                        candle = marketdata.candle
+                        if data_tasks_long5[market]['tickers'][candle.figi]['cur_atr']['time_received'] != None:
+                            if data_tasks_long5[market]['tickers'][candle.figi]['prev_bin'] != (candle.last_trade_ts.minute // 5): # Проверка перехода между 5 мин
+                                # произошёл переход 5 мин
+
+                                # обновляем массив atr
+                                data_tasks_long5[market]['tickers'][candle.figi]['atr'].append(
+                                    data_tasks_long5[market]['tickers'][candle.figi]['cur_atr']['high'] -
+                                    data_tasks_long5[market]['tickers'][candle.figi]['cur_atr']['low']
+                                )
+                                if len(data_tasks_long5[market]['tickers'][candle.figi]['atr']) > 5:
+                                    del data_tasks_long5[market]['tickers'][candle.figi]['atr'][0]
+
+                                # инициализируем cur_atr для новой 5 мин
+                                try:
+                                    data_tasks_long5[market]['tickers'][candle.figi]['cur_atr']['high'] = float(quotation_to_decimal(candle.high))
+                                    data_tasks_long5[market]['tickers'][candle.figi]['cur_atr']['low'] = float(quotation_to_decimal(candle.low))
+                                    data_tasks_long5[market]['tickers'][candle.figi]['prev_bin'] = (candle.last_trade_ts.minute // 5)
+                                except ValueError:
+                                    print(f"ERROR stream_list_figi_five_minute({market}): except ValueError: candle = {candle}", flush=True)
+                            else:
+                                if float(quotation_to_decimal(candle.high)) > data_tasks_long5[market]['tickers'][candle.figi]['cur_atr']['high']:
+                                    data_tasks_long5[market]['tickers'][candle.figi]['cur_atr']['high'] = float(quotation_to_decimal(candle.high))
+                                if float(quotation_to_decimal(candle.low)) < data_tasks_long5[market]['tickers'][candle.figi]['cur_atr']['low']:
+                                    data_tasks_long5[market]['tickers'][candle.figi]['cur_atr']['low'] = float(quotation_to_decimal(candle.low))
+                            data_tasks_long5[market]['tickers'][candle.figi]['cur_atr']['time_received'] = candle.last_trade_ts
+                        else:
+                            try:
+                                # first initialization
+                                data_tasks_long5[market]['tickers'][candle.figi]['cur_atr']['high'] = float(quotation_to_decimal(candle.high))
+                                data_tasks_long5[market]['tickers'][candle.figi]['cur_atr']['low'] = float(quotation_to_decimal(candle.low))
+                                data_tasks_long5[market]['tickers'][candle.figi]['cur_atr']['time_received'] = candle.last_trade_ts
+                                data_tasks_long5[market]['tickers'][candle.figi]['prev_bin'] = (candle.last_trade_ts.minute // 5)
+                            except ValueError:
+                                print(f"ERROR stream_list_figi_five_minute({market}): except ValueError: candle = {candle}", flush=True)
+
+                        if not data_tasks_long5[market]['depends']:
+                            # Задача по market завершена - завершаем опрос
+                            should_unsubscribe = True
+                            data_tasks_long5.pop(market, None)
+                            await asyncio.sleep(2)              # ожидание завершения should_unsubscribe
+                            break
+    except Exception as e:
+        print(f"ERROR critical stream_list_figi_five_minute({market}): {e}", flush=True)
